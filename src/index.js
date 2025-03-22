a6db29097b3314f12f81ae6bf6f152556227f2a2
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-03-22 22:55:55 +0530

    on day 8, in utils folder three files are created for api error hadling, request, response. app.js was also added and coded in this session. install dep like 'cookie-parser' 'cors'

--- src/index.js
+++ src/index.js
@@ -7,19 +7,22 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port: ${process.env.PORT}`);  
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!", err);    
+})
+
+
+
+
+
+
 // require('dotenv').config({path: './env'}) 
 // 2 met for env if have to use import  statement
-
-
-
-
-
-
-
-
-
-
-
 
 
 //   import {DB_NAME} from "./constants.js"; use hi nahi kar rah (app 1)
