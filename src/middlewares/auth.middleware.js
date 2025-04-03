38d96b9a45f7f71963339b5d5d8110cc0f6ce678
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-04-03 15:54:33 +0530

    Implement user authentication with login, logout, and token refresh functionality In day 16, we learn the concept of tokens. Note: code can have small bugs. connection should have to established before using postman.

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+try {
+      const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", "");
+    
+      if (!token) {
+        throw new ApiError(401, "Unauthorized request");
+      }
+    
+      const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+      const user = await User.findById(decodeToken?._id).select(
+        "-password -refreshToken"
+      );
+    
+      if (!user) {
+        // todo: disucss about frontend
+        throw new ApiError(401, "Invalid access");
+      }
+    
+      req.user = user;
+      next();
+} catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access")
+}
+});
+
+export {verifyJWT}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,32 @@
-import { Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
-
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1,
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-export default router
+rotuer.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+export default router;
