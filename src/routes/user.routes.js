44a9f0823575ebaa1093d7d2dfd5b6f57b2c4dc0
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-04-05 00:01:15 +0530

    Fix typos in route definitions and update user routes for account management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changCurrentPassword,
+  getCurrentUser,
+  updateAccoutdetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -29,4 +37,12 @@ rotuer.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+rotuer.route("/update-account").patch(verifyJWT, updateAccoutdetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverimage").patch(verifyJWT, upload.single("/coverimage"), updateUserCoverImage)
+rotuer.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+rotuer.route("/history").get(verifyJWT, getWatchHistory)
 export default router;
